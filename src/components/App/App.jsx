949e4d3a8cdd26a461fdccda9bd6f4aa669f4915
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-08-12 20:57:27 -0500

    GET request done in fetchGallery

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+
+    useEffect(() =>{
+      fetchGallery();
+    }, []);
+
+    const fetchGallery = () => {
+      axios({
+        method: 'GET',
+        url: '/Gallery/'
+      }).then ( response => {
+        console.log('data from GET', response);
+      }).catch( err => {
+        console.log('client GET', err);
+      })
+    }//end of fetchGallery
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,6 +29,6 @@ function App() {
         <img src="images/goat_small.jpg"/>
       </div>
     );
-}
+} //end of App Function
 
 export default App;
